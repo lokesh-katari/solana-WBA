d93ee9826d6293fac6d10670a541ef4fc0caa34a
lokesh-katari <lokeshkatari921@outlook.com>
2024-05-15 18:17:20 +0530

    minted token on solana

---
+++ ts/cluster1/spl_init.ts
@@ -0,0 +1,29 @@
+import { Keypair, Connection, Commitment } from "@solana/web3.js";
+import { createMint } from "@solana/spl-token";
+import { wallet } from "../dev-wallet.json";
+
+// Import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+// const keypair = Keypair.generate();
+// console.log(`Keypair: ${keypair.secretKey}`);
+// console.log(`Keypair: ${keypair.publicKey.toString()}`);
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+(async () => {
+  try {
+    // Start here
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      null,
+      6
+    );
+    console.log(`Mint: ${mint}`);
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
